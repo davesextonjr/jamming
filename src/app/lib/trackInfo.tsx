611e64ce72f1cfe4651fff9fdd0e70850efd61a7
David Sexton Jr <davesextonjr@gmail.com>
2024-03-05 15:14:31 -0500

    add sample tracks

---
+++ src/app/lib/trackInfo.tsx
@@ -0,0 +1,28 @@
+import { Track } from './definitions';
+export const sampleTracks: Track[] = [
+    {
+        name: 'track name 1',
+        artist: 'artist 1',
+        album: 'album 1'
+    },
+    {
+        name: 'track name 2',
+        artist: 'artist 2',
+        album: 'album 2'
+    },
+    {
+        name: 'track name 3',
+        artist: 'artist 3',
+        album: 'album 3'
+    },
+    {
+        name: 'track name 4',
+        artist: 'artist 4',
+        album: 'album 4'
+    },
+    {
+        name: 'track name 5',
+        artist: 'artist 5',
+        album: 'album 5'
+    }
+]
